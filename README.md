c22a3791835447547f322eaf85be3e6f706214d6
Vlad Ioan Topan <vtopan/gmail>
2018-12-19 01:48:19 +0200

    Added support for getting currency sell prices

--- README.md
+++ README.md
@@ -64,6 +64,11 @@ an item in-game - this is a standard game feature!) and queries [pathofexile.com
 lowest 20 prices for which that item is listed (useful to get an idea if that random unique is
 now worth $$$ exalts).
 
+*Update*: added support for retrieving currency rates as well (if you *Ctrl+C* over e.g. an *Exalted
+Orb* you get a list of what it's selling for). The `currency-map.txt` file contains name mappings
+from the full / in-game name to the random-like shortcuts used by the site ("Perandus Coin" is
+"p"?!), but it's only partially filled in.
+
 **Note:** I am not sure whether the trade API (not the site, but the actual internal API used by
 the site) is intended to be used in tools. If not, please let me know & I'll remove the offending
 code.
--- poe_watchclip.py
+++ poe_watchclip.py
@@ -5,6 +5,7 @@ Watch clipboard for unique items & query PoE official trade site for lowest pric
 Author: Vlad Ioan Topan (vtopan/gmail)
 """
 
+import os
 import re
 import pprint
 from tkinter import Tk  # Windows only, for easy clipboard access
@@ -16,6 +17,14 @@ import requests
 VER = '0.1.0 (dec.2018)'
 
 PRICE_COUNT = 20    # number of (lowest) prices to retrieve
+CURR_MAP_FILE = 'currency-map.txt'
+
+# needed to map from the full names to the random-like shortcuts
+if os.path.isfile(CURR_MAP_FILE):
+    CURR_MAP = {e[0].strip():e[1].strip() for e in re.findall(r'([ \w]+)\s*=\s*(\w+)',
+        open(CURR_MAP_FILE).read())}
+else:
+    CURR_MAP = {'Exalted Orb': 'exa'}
 
 
 def parse_item_info(text):
@@ -26,6 +35,8 @@ def parse_item_info(text):
     if not m:
         return {}
     info = {'name': m[0][1], 'rarity': m[0][0], 'type': m[0][2]}
+    if info['rarity'] == 'Currency':
+        info['type'] = info.pop('rarity')
     m = re.findall(r'^Quality: +(\d+)%', text)
     info['quality'] = int(m[0]) if m else 0
     m = re.findall(r'^Sockets: ((?:\w-){5,})', text)
@@ -35,8 +46,26 @@ def parse_item_info(text):
     return info
 
 
-def query_trade(name=None, links=None, corrupted=None, rarity=None, league='Betrayal', priced=None,
-            max_res=10):
+def fetch_trade_results(ids, exchange=False):
+    """
+    Fetch the trade/exchange search results for the given ID list.
+    """
+    results = []
+    if ids:
+        # can only get max. 10 results in a request
+        for i in range(0, PRICE_COUNT, 10):
+            url = f'https://www.pathofexile.com/api/trade/fetch/{",".join(ids[i:i+10])}'
+            # print(f'[#] Requesting {url}...')
+            res = requests.get(url)
+            if res.status_code != 200:
+                print(f'[!] Trade result retrieval failed: HTTP {res.status_code}! '
+                        f'Message: {res.json().get("error", "unknown error")}')
+                break
+            results += res.json()['result']
+    return results
+
+
+def query_trade(name=None, links=None, corrupted=None, rarity=None, league='Betrayal', priced=None):
     """
     Query pathofexile.com/trade.
 
@@ -70,20 +99,30 @@ def query_trade(name=None, links=None, corrupted=None, rarity=None, league='Betr
     jres = res.json()
     if res.status_code != 200:
         print(f'[!] Trade query failed: HTTP {res.status_code}! '
-                'Message: {jres.get("error", "unknown error")}')
-        return {}
-    if jres['result']:
+                f'Message: {jres.get("error", "unknown error")}')
+        return []
+    results = fetch_trade_results(jres['result'])
+    return results
+
+
+def query_exchange(currency, league='Betrayal'):
+    """
+    Query pathofexile.com/trade/exchange for how much a currency sells.
+    """
+    qcurrency = CURR_MAP.get(currency, currency.replace(' ', '-').replace("'", '').lower())
+    j = {'exchange': {'have': ['chaos'], 'want': [qcurrency], 'status': {'option': 'online'}}}
+    # print('[#] Query parameters:')
+    # pprint.pprint(j)
+    res = requests.post(f'https://www.pathofexile.com/api/trade/exchange/{league}', json=j)
+    jres = res.json()
+    if res.status_code != 200:
+        print(f'[!] Trade query failed: HTTP {res.status_code}! '
+                f'Message: {jres.get("error", "unknown error")}')
+        return []
+    results = fetch_trade_results(jres['result'], exchange=True)
+    if results and results[0]['item']['typeLine'] != currency:
         results = []
-        # can only get max. 10 results in a request
-        for i in range(0, max_res, 10):
-            url = f'https://www.pathofexile.com/api/trade/fetch/{",".join(jres["result"][i:i+10])}'
-            # print(f'[#] Requesting {url}...')
-            res = requests.get(url)
-            if res.status_code != 200:
-                print(f'[!] Trade result retrieval failed: HTTP {res.status_code}! '
-                        'Message: {res.json().get("error", "unknown error")}')
-                break
-            results += res.json()['result']
+        print(f'[!] Failed translating {currency} to short name; add it to currency-map.txt!')
     return results
 
 
@@ -98,19 +137,26 @@ def watch_clipboard():
             text = Tk().clipboard_get()
             if text != prev:
                 info = parse_item_info(text)
-                if info and info['rarity'] == 'Unique':
-                    print('[*] Found unique item in clipboard: %(name)s (%(type)s)' % info)
-                    # pprint.pprint(info)
-                    print('[-] Getting prices from pathofexile.com/trade...')
-                    tinfo = query_trade(priced=True, max_res=PRICE_COUNT, **{k:v for k, v in
-                            info.items() if k in ('name', 'links', 'corrupted', 'rarity')})
-                    if not tinfo:
-                        print('[!] No results!')
-                    else:
-                        prices = [x['listing']['price'] for x in tinfo]
+                # pprint.pprint(info)
+                trade_info = None
+                if info:
+                    if info.get('rarity') == 'Unique':
+                        print('[*] Found unique item in clipboard: %(name)s (%(type)s)' % info)
+                        print('[-] Getting prices from pathofexile.com/trade...')
+                        trade_info = query_trade(priced=True,
+                                **{k:v for k, v in info.items() if k in ('name', 'links',
+                                'corrupted', 'rarity')})
+                    elif info['type'] == 'Currency':
+                        print(f'[-] Found currency {info["name"]} in clipboard; '
+                                'getting prices from pathofexile.com/trade/exchange...')
+                        trade_info = query_exchange(info['name'])
+                    if trade_info:
+                        prices = [x['listing']['price'] for x in trade_info]
                         prices = ['%(amount)s%(currency)s' % x for x in prices]
                         prices = {'%s x %s' % (prices.count(x), x):None for x in prices}
                         print(f'[-] Lowest {PRICE_COUNT} prices: {", ".join(prices.keys())}')
+                    elif trade_info is not None:
+                        print(f'[!] No results!')
                 prev = text
             time.sleep(.3)
         except KeyboardInterrupt:
